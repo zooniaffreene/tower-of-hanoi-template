e268d4f10c610f13795d3bfce84865216cd620e8
zooniaffreene <zooniaffreene1073@gmail.com>
2025-03-28 23:19:53 -0700

    Update TowerModel.java

--- TowerModel.java
+++ TowerModel.java
@@ -1,88 +1,58 @@
 public class TowerModel {
 
-    // 2d array storing game state
     private IntegerStack[] towers;
+    private int towerHeight;
+    private int moveCounter;
 
-    // Final tower height
-    private int towerHeight = 0;
-
-    // Debug metrics
-    private int printCounter = 0;
-    private int moveCounter = 0;
-
-    /* This class implements a model of a tower of Hanoi game.
-
-        |    |    |
-        =    |    |
-       ===   |    |
-      =====  |    |
-     ------------------
-        0    1    2
-
-     Example of a game of height three in the starting position.
-
-    */
-    public TowerModel(int height)
-    {
+    public TowerModel(int height) {
         towerHeight = height;
         towers = new IntegerStack[3];
-        towers[0] = new IntegerStack(height);
-        towers[1] = new IntegerStack(height);
-        towers[2] = new IntegerStack(height);
-
-        for (int i=0; i<height; i++)
-        {
-            towers[0].push(height - i);
+        for (int i = 0; i < 3; i++) {
+            towers[i] = new IntegerStack(height);
+        }
+        for (int i = height; i > 0; i--) {
+            towers[0].push(i);
         }
     }
 
-    // get the total number of disks
-    public int height()
-    {
+    public int height() {
         return towerHeight;
     }
 
-
-    // Move one disk from the source stack to the destination stack.
-    public void move(int source, int destination)
-    {
-        System.out.println("Move #" + ++moveCounter + " from " + source + " to " + destination);
-        // TODO!!
+    public void move(int source, int destination) {
+        if (!towers[source].isEmpty() && (towers[destination].isEmpty() || towers[source].peek() < towers[destination].peek())) {
+            int disk = towers[source].pop();
+            towers[destination].push(disk);
+            moveCounter++;
+            System.out.println("Move " + disk + " from " + source + " to " + destination);
+        } else {
+            System.out.println("Invalid move from " + source + " to " + destination);
+        }
     }
 
-    // Helper method to nicely print the current model state.
-    public void print()
-    {
-        System.out.print("Print #" + ++printCounter + " Towers of Hanoi\n");
-
-        for (int layer = towerHeight-1; layer >= 0; layer--){
-            System.out.print("\n");
-            for (int tower = 0; tower < towers.length; tower++) {
-                int value = towers[tower].get(layer);
-                if (value == 0)
-                {
-                    System.out.print(" |");
-                } else
-                {
-                    System.out.print(" " + value);
-                }
-            }
-        }
-        System.out.println();
-        for (int i = 0; i < towers.length; i++) {
-            System.out.print(" =");
-        }
-        System.out.println();
-    }
-
-    // Test instrumentation
-    public IntegerStack[] getTowers()
-    {
+    public IntegerStack[] getTowers() {
         return towers;
     }
+}
 
-    public int getHeight()
-    {
-        return towerHeight;
+public class TowerSolver {
+    private TowerModel model;
+
+    public TowerSolver() {}
+
+    public void solve(TowerModel model) {
+        this.model = model;
+        moveDisks(model.height(), 0, 2, 1);
+    }
+
+    private void moveDisks(int n, int from, int to, int extra) {
+        if (n == 1) {
+            model.move(from, to);
+        } else {
+            moveDisks(n - 1, from, extra, to);
+            model.move(from, to);
+            moveDisks(n - 1, extra, to, from);
+        }
     }
 }
+
